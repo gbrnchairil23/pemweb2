f9d281082196f8f94981cc351089464ac549a08e
gbrnchairil23_ <chairilgibran31@gmail.com>
2023-02-28 12:25:24 +0700

    upload file

---
+++ coba1.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('PHI', 3.14);
+    define('DBNAME', 'inventori');
+    define('DBSERVER', 'localhost');
+
+    echo 'Nilai Phi : ' . PHI;
+    echo '<br>Nama Database : ' . DBNAME;
+    echo '<br>Lokasi Database : ' . DBSERVER;
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nama = 'Gibran';
+    $umur = 19;
+    $berat = 75;
+
+    echo 'Nama Saya : ' . $nama;
+    echo '<br>Umur Saya : ' . $umur;
+    echo '<br>Umur Saya : ' . $berat;
